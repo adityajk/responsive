991c5caf93db239bb9b7ba4ea534df2ab1f428aa
Aditya <aditya05khajuria@gmail.com>
2015-02-18 23:19:55 +0530

    Added carousel

--- js/dashboard/controllers/master-ctrl.js
+++ js/dashboard/controllers/master-ctrl.js
@@ -5,10 +5,25 @@ angular.module('Dashboard')
     .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
 
 function MasterCtrl($scope, $cookieStore) {
-    /**
+     $scope.myInterval = 5000;
+      var slides = $scope.slides = [];
+      $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+          image: 'http://placekitten.com/' + newWidth + '/300',
+          text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+            ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+      };
+      for (var i=0; i<4; i++) {
+        $scope.addSlide();
+      }
+     
+      /**
      * Sidebar Toggle & Cookie Control
      *
      */
+     
     var mobileView = 992;
 
     $scope.getWidth = function() { return window.innerWidth; };
